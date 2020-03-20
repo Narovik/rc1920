263585b01c938daeb8f9d8ad3da63cefabe79481
victornarov <victornarov@gmail.com>
2020-03-20 12:11:50 +0100

    Responder preguntas 1,2,3

--- invertir_lista.pl
+++ invertir_lista.pl
@@ -1,1 +1,12 @@
-% I
+% Invertir lista en Prolog
+%
+% invertir(+Lista, -Resultado)
+%	Es cierto si Resultado unifica con una lista que
+% contiene los mismos elementos de la lista en orden inverso
+%
+
+
+invertir([],[]).								% P(n0)
+invertir([Cabeza|Resto], R2) :-		% P(n-1) -> P(n)
+		invertir(Resto, R),
+		append(R, [Cabeza], R2).
