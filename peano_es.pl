da9f67826b8b378dff4f2df317624f9dd3882b68
victornarov <victornarov@gmail.com>
2020-03-30 13:06:46 +0200

    División en Peano

--- peano_es.pl
+++ peano_es.pl
@@ -58,5 +58,50 @@ multiplica(_, 0, 0).
 %multiplica(N1, s(0), N1).
 multiplica(N1, s(N2), R)  :- multiplica(N1, N2, R1), suma(N1,R1,R).
 
+
+/* divide(?N1, ?N2, ?R, ?Resto)
+	es cierto si R unifica con el número 
+	de veces que puede restarse N2 a N1.
+	Resto unificará con el resto de la división entera.
+	
+	divide(s(s(s(0))), s(s(0)), R, Resto). % 3/2
+	
+	R=s(0).		% Cociente 1
+	Resto=s(0).  % Resto 1
+	
+	------
+	
+	2	5	->	R=0, Resto=2.
+	
+	5	2	->	R=2, Resto=1.
+	
+	3	2	->	R=1, Resto=1.
+	
+*/
+divide(N1, N2, 0, N1):- menor(N1, N2).
+
+divide(N1, N2, s(R), Resto):-
+resta(N1, N2, N12),
+divide(N12, N2 , R, Resto).
+  
+*/ Necistamos una funcion axuliar:
+	menor(?N1, ?N2).
+	es cierto si N1 es menor que N2 en
+	aritmética de Peano.
+	
+	Ejecución:
+	menor(s(s(s(0))), s(s(s(s(3))))).
+	menor(s(s(0)), s(s(s(0)))).
+	menor(s(0), s(s(0))).
+	menor(0, s(0)).
+	true.
+*/
+
+menor(0, s(_)).
+
+menor(s(N1),  s(N2)) :-
+	menor(N1,  N2).
+
+	
 	
 
