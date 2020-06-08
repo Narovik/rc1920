5425c1ca05c3fc33964de74664f9485e1bdfd55d
Víctor M. Rodríguez <victornarov@gmail.com>
2020-06-08 09:01:51 +0200

    Actualizar, reordenar y subir trabajo

--- estados-jarras.pl
+++ estados-jarras.pl
@@ -15,6 +15,38 @@
     -------    -----
 	  G5         G3
 	  
+	           |---|T
+	|     |    |   |_ 
+	|     |    |   | 
+	|     |    |   |
+    -------    -----
+	  G5         G3
+   
+    Pasar de G5 a G3
+    1. T is G5 + G3, T =< 3
+       G5 -> 0
+       G3 -> T
+    2. T > 3
+	   G3 -> 3
+       G5 -> T - 3	
+	  
+	  
+	           
+	|     |    
+	|     |    |   | 
+	|     |    |   |
+    -------    -----
+	  G5         G3
+	  
+	Pasar de G3 a G5
+    1. T =< 5
+	   G3 -> 0
+	   G5 -> T
+	5. T > 5
+       G5 -> 5
+       G3 -> T - 5	   
+    	
+	  
    1. Definir el estado
    
       estado(G5, G3)
@@ -43,9 +75,7 @@
         4. vaciar G3
         5. pasar de G3 a G5
         6. pasar de G5 a G3 		
-
-
-
+		
 mov(Nombre, EstadoAntes, EstadoDespués)
 
 */
@@ -56,30 +86,47 @@ mov(Nombre, EstadoAntes, EstadoDespués)
  /* 2. llenar G3 */
  mov(llenarG3, estado(G5, _), estado(G5, 3)).
  
+ 
 /* 3. vaciar G5 */
 mov(vaciarG5, estado(_, G3), estado(0, G3)).
  
 /* 4. vaciar G3 */
 mov(vaciarG3, estado(G5, _), estado(G5, 0)).
- 
+  
 /* 5. pasar de G3 a G5 */
-mov(pasarG3G5, estado(G5, G3),  estado(nuevoG5, 0)) :- 
-	NuevoG5 is G5 + G3, 
-	NuevoG5 <= 5.
-	
-mov(pasarG3G5, estado(G5, G3),  estado(5, nuevoG3)) :- 
-	NuevoG5 is G5 + G3,  
-	NuevoG5 > 5, 
-	NuevoG3 is NuevoG5 - 5.
+mov(deG3aG5, estado(G5, G3), estado(T, 0)):-
+  T is G3 + G5, T =< 5.
+mov(deG3aG5, estado(G5, G3), estado(5, G32)):-
+  T is G3 + G5, T > 5,
+  G32 is T - 5.
 
 /* 6. pasar de G5 a G3 */
-mov(pasarG5G3, estado(G5, G3),  estado(0, nuevoG3)) :- 
-	NuevoG3 is G3 + G5, 
-	NuevoG3 <= 3.
-	
-mov(pasarG5G3, estado(G5, G3),  estado(nuevoG5, 3)) :- 
-	NuevoG3 is G3 + G5, 
-	NuevoG3 > 3, 
-	NuevoG5 is NuevoG3 - 3.
+mov(deG5aG3, estado(G5, G3), estado(0, T)):-
+  T is G5 + G3,
+  T =< 3.
+  
+mov(deG5aG3, estado(G5, G3), estado(G52, 3)):-
+  T is G5 + G3,
+  T > 3,
+  G52 is T - 3.
+  
+/* camino(+EstadoInicial, +EstadoFinal, +Visitados, -R).
+   es cierto si R unifica con una lista de movimientos
+   para pasar del EstadoInicial al EstadoFinal sin
+   repetir los estados de la lista de estados
+   Visitados.
+   
+   Inicial -> Mov -> EstadoTMP              Final
+                        --------------------->
+						         N-1 
+*/
 
+camino(Ini, Ini, _, []).
 
+camino(Ini, Fin, Visitados, [Mov|Camino]):-
+  mov(Mov, Ini, Tmp),
+  \+ member(Tmp, Visitados),
+  camino(Tmp, Fin, [Tmp|Visitados], Camino).
+  
+
+  
